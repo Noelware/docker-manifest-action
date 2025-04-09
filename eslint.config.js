bc3cb27c300ddc7b917a143d5cf3413aaf34bcee
Noel <cutie@floofy.dev>
2025-04-09 12:20:44 -0700

    Fix OOP error when running `bun lint`

--- eslint.config.js
+++ eslint.config.js
@@ -24,9 +24,14 @@
 import noel from '@augu/eslint-config';
 import { fileURLToPath } from 'node:url';
 
-export default noel({
-    stylistic: true,
-    typescript: {
-        tsconfig: fileURLToPath(new URL('./tsconfig.json', import.meta.url))
+export default noel(
+    {
+        stylistic: true,
+        typescript: {
+            tsconfig: fileURLToPath(new URL('./tsconfig.json', import.meta.url))
+        }
+    },
+    {
+        ignores: ['build/**']
     }
-});
+);
